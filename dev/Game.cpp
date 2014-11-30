0a6d7a5059c716428506f172d2b57e56def3953f
Beo4ever <Beo4ever@users.noreply.github.com>
2014-11-30 21:31:39 +0300

    Remove Phys.cpp, Phys.hpp

--- dev/Game.cpp
+++ dev/Game.cpp
@@ -20,12 +20,9 @@ Game::Game() : GameBattlefield("Game Implementation") {}
 //Ниже реализуются базовые функции любой сетевой игры,
 //при необходимости вы можете дополнить их своими функциями
 
-
 void Game::Join(GameParticipantPtr participant)
 {
     _participants.insert(participant);
-    for_each(_recentMsgs.begin(), _recentMsgs.end(),
-                  boost::bind(&GameParticipant::Deliver, participant, _1));
 }
 
 /*
@@ -38,6 +35,6 @@ void Game::Leave(GameParticipantPtr participant)
 void Game::Deliver(const GameMessage &msg)
 {
     for_each(_participants.begin(), _participants.end(),
-                  boost::bind(&GameParticipant::Deliver, _1, boost::ref(msg)));
+             boost::bind(&GameParticipant::Deliver, _1, boost::ref(msg)));
 }
 
