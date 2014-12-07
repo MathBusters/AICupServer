f930310bafd30348b8ec7cd206a1762d0d9e3f3b
Beo4ever <Beo4ever@users.noreply.github.com>
2014-12-07 21:23:54 +0300

    Cleaned comments

--- AICupApp.cpp
+++ AICupApp.cpp
@@ -19,8 +19,6 @@ using namespace AICup;
 using namespace AICup::Core;
 using namespace AICup::Dev;
 
-//typedef boost::shared_ptr<ip::tcp::socket> socket_ptr;
-
 AICupApp::AICupApp(int port) : _port(port)
 {
 }
@@ -36,7 +34,6 @@ int AICupApp::Run()
         boost::asio::io_service io_service;
 
         tcp::endpoint endpoint(tcp::v4(), _port);
-//        boost::shared_ptr<GameServer> server(new GameServer(io_service, endpoint));
         Game game;
         GameServer server(io_service, endpoint, game);
         cout << "GameServer (" << game.GetName() << ") Started." << endl;
